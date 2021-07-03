02ce1bfc9c0571db190efa53f1e415cbb17b5941
Belvatd <belva_talitha_28rpl@telkomschools.sch.id>
2021-07-03 20:17:32 +0700

    routing

---
+++ src/pages/Register.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Register extends Component {
+    render() {
+        return (
+            <div>
+                hai ini regis
+                <h1>reguss</h1>
+            </div>
+        )
+    }
+}
