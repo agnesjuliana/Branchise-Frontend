86ae13014bcc20d46da13d919031aff22f3dfaa0
Agnes Juliana <agnesjuliana26@gmail.com>
2021-07-05 21:59:57 +0700

    Image

--- src/components/FranchiseCard.js
+++ src/components/FranchiseCard.js
@@ -7,7 +7,7 @@ export default class FranchiseCard extends React.Component {
         return (
             <div className="col-lg-3 col-md-6 my-2">
                 <div className="card franchiseCard">
-                    <img className="franchiseImage" src="https://64.media.tumblr.com/211fe4ca7d8e4b79ff6f25787f7bb734/68476a66f03a1bc9-2a/s1280x1920/0eb9e946c96bf19e089ec1229ffc921275f880d1.jpg" alt="franchiseImage" />
+                    <img className="franchiseImage" src={franchise_image_url + this.props.pict} />
                     <div className="overlay">
                         <div className="textFranchise">{this.props.name}</div>
                     </div>
--- src/config.js
+++ src/config.js
@@ -1,11 +1,13 @@
-const base_url = "http://localhost:8000/branchise"
-const customer_image_url = "http://localhost:8000/img/img_customer"
-const founder_image_url = "http://localhost:8000/img/img_founder"
-const franchise_image_url = "http://localhost:8000/img/img_franchise"
+// const base_url = "http://localhost:8000/branchise"
+const base_url = "https://branchise.moklet.io/api"
+const base_img = "https://branchise.moklet.io"
+// const customer_image_url = "http://localhost:8000/img/img_customer"
+// const founder_image_url = "http://localhost:8000/img/img_founder"
+const franchise_image_url = `${base_img}/img_franchise/`
 
 export{
     base_url,
-    customer_image_url,
-    founder_image_url,
+    // customer_image_url,
+    // founder_image_url,
     franchise_image_url
 }
