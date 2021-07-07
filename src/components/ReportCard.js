ac6699fc66fbc46936c92d9696053cb602c6495f
Agnes Juliana <agnesjuliana26@gmail.com>
2021-07-07 20:25:11 +0700

    laporan finishing

--- src/components/ReportCard.js
+++ src/components/ReportCard.js
@@ -6,11 +6,11 @@ export default class ReportCard extends React.Component {
         return (
             <div className={"card reportCard "+this.props.color}>
             <div className="row">
-                <div className="col-lg-10">
+                <div className="col-10">
                     <h4 className="reportCardTitle my-2 mx-3">{this.props.title}</h4>
                     <img className="mx-3 metered" src={"https://drive.google.com/uc?id="+this.props.pict} alt="central" />
                 </div>
-                <div className="col-lg-2">
+                <div className="col-2">
                     <div className="cardCount my-2 pt-2">
                         <h3>{this.props.count}</h3>
                     </div>
--- src/pages/Laporan.js
+++ src/pages/Laporan.js
@@ -9,19 +9,23 @@ export default function Page() {
             <SideBar>
                 <>
                     <div className="row">
-                        <div className="col-lg-7">
+                        <div className="col-lg-7 mx-3">
                             <h1>Laporan Keseharian</h1>
                             <h3 className="dateReport">9 July, 2021</h3>
 
                             <div className="reportCardWrap">
                                 <ReportCard title="Milk Base" pict="1NoAbwBH0IYJMLaJl1yRDxIFnkna-bkO6" count="13" color="blue"/>
-                                <ReportCard title="Coffe Base" pict="1NoAbwBH0IYJMLaJl1yRDxIFnkna-bkO6" count="50" color="purple"/>
-                                <ReportCard title="Soda" pict="1NoAbwBH0IYJMLaJl1yRDxIFnkna-bkO6" count="20" color="red"/>
-                                <ReportCard title="Juice" pict="1NoAbwBH0IYJMLaJl1yRDxIFnkna-bkO6" count="80" color="yellow"/>
+                                <ReportCard title="Coffe Base" pict="1HmAZfVRm7tMd3ANWrSeNsa0Z-rVtnr8q" count="50" color="purple"/>
+                                <ReportCard title="Soda" pict="1Vswg2ipb97Qaln7i-0dbkW1XwmDEBj-P" count="20" color="red"/>
+                                <ReportCard title="Juice" pict="1QSbdlEFjkn7-_RDlJwLs0N72g36D1v4V" count="80" color="yellow"/>
                             </div>
                         </div>
-                        <div className="col-lg-4 mx-5">
-
+                        <div className="col-lg-4 mx-3">
+                            <div className="kalenderCard">
+                                <h5 className="">Progress Target</h5>
+                                <img className="targetImg" src="https://drive.google.com/uc?id=10wkCCy0Mlib_Ep38Q7ODVHsisFFt0kZm" />
+                                <img className="kalenderImg" src="https://drive.google.com/uc?id=1Xdr-0XKBt5WVXI0pkhpVMona5DlZesbD" />
+                            </div>
                         </div>
                     </div>
                 </>
